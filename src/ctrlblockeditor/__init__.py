1eaa7932155752bb7fa3f07d2ffd8e0dce36a774
pengmanhht <manpenghht@gmail.com>
2024-12-22 15:29:06 +0100

    init CtrlBlockEditor project

---
+++ src/ctrlblockeditor/__init__.py
@@ -0,0 +1,10 @@
+r"""
+===============
+CtrlBlockEditor
+===============
+
+CtrlBlockEditor is a Python package for creating and editing $BLOCK in NONMEM control file.
+
+"""
+
+__version__ = '0.1.0'
